c5db05895836a3e54f9305a29bdaef2040b54982
Jack Workman <jcworkma@asu.edu>
2014-11-03 15:30:22 -0700

    Limited NERTagger return to Locations only.

--- geolocator/app/nlp_magic.py
+++ geolocator/app/nlp_magic.py
@@ -13,19 +13,29 @@ def stanford_nlp_location_magic(file_text):
 
 	import os
 
+	locations = []
+	LOCATION = u'LOCATION'
+
 	# --- command to get executing directory
 	# return os.getcwd()
 
 	# --- attempt to configure java with python
 	os.environ["JAVAHOME"] = '/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin' 
-	# return str(os.environ)
-
-	# --- attempt to configure java with nltk
-	# nltk.internals.config_java('../usr/lib/jvm/default-java/jre/bin/java')
-	# nltk.internals.config_java('/usr/bin/java')
 
 	# --- example found here: www.nltk.org/api/nltk.tag.html#module-nltk.tag.stanford
-	# --- does not currently work
 	st = NERTagger('geolocator/app/static/stanford-ner-2014-08-27/classifiers/english.all.3class.distsim.crf.ser.gz', 'geolocator/app/static/stanford-ner-2014-08-27/stanford-ner.jar')
+	stanford_magic = []
+	# split = file_text.split()
+	punctuations = ['.', '!', '?']
+	file_text = file_text.replace(punctuations[0], ' ')
+	file_text = file_text.replace(punctuations[1], ' ')
+	file_text = file_text.replace(punctuations[2], ' ')
 	stanford_magic = st.tag(file_text.split())
-	return str(stanford_magic)
+	for x in xrange(len(stanford_magic)):
+		if stanford_magic[x][1] == LOCATION:
+			if stanford_magic[x+1][1] == LOCATION:
+				locations.append(stanford_magic[x][0] + ' ' + stanford_magic[x+1][0])
+				x += 1
+			else:
+				locations.append(stanford_magic[x][0])
+	return str(locations)
