9d59e91003dc1bdcf776dc75ee89bdb6f4af7e88
Weston Neal <wtneal@asu.edu>
2014-11-22 22:50:23 -0700

    Added DB import scripts

---
+++ db/imports/getsources.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Create the temp directorys
+mkdir -p ~/geodata
+cp -f ./importdata.sql ~/geodata/
+cd ~/geodata
+
+# Download the dump files
+curl -O http://download.geonames.org/export/dump/featureCodes_en.txt 
+curl -O http://download.geonames.org/export/dump/readme.txt 
+curl -O http://download.geonames.org/export/dump/countryInfo.txt
+curl -O http://download.geonames.org/export/dump/allCountries.zip
+
+# Extract the compressed files
+unzip allCountries.zip
+rm allCountries.zip
+
+# Create the tables
+psql -h localhost -p 5432 -d app -U postgres -a -f ~/geodata/importdata.sql
+
+# clean up
+rm -rf ~/geodata
---
+++ db/imports/importdata.sql
@@ -0,0 +1,82 @@
+/* Create the temp tables to import the data to */
+DROP TABLE raw_country_info;
+DROP TABLE raw_feature_codes;
+DROP TABLE raw_locations;
+
+CREATE TABLE raw_country_info (
+	ISO varchar,
+	ISO3 varchar,
+	ISONumeric varchar,
+	fips varchar,
+	country varchar,
+	capital varchar,
+	area varchar,
+	population varchar,
+	continent varchar,
+	tld varchar,
+	currencycode varchar,
+	currencyname varchar,
+	phone varchar,
+	postalcodeformat varchar,
+	postalcoderegex varchar,
+	languages varchar,
+	geonameid varchar,
+	neighbours varchar,
+	equivalentfipscode varchar
+);
+
+CREATE TABLE raw_feature_codes (
+	code varchar,
+	name varchar,
+	description varchar
+);
+CREATE TABLE raw_locations (
+	geonameid varchar,
+	name varchar,
+	asciiname varchar,
+	alternamtename varchar,
+	latitude varchar,
+	longitude varchar,
+	featureclass varchar,
+	featurecode varchar,
+	countrycode varchar,
+	cc2 varchar,
+	admin1code varchar,
+	admin2code varchar,
+	admin3code varchar,
+	admin4code varchar,
+	population varchar,
+	elevation varchar,
+	dem varchar,
+	timezone varchar,
+	modificationdate varchar
+);
+
+
+/* Import the data */
+\COPY raw_country_info FROM '/tmp/geodata/countryInfo.txt';
+\COPY raw_feature_codes FROM '/tmp/geodata/featureCodes_en.txt';
+\COPY raw_locations FROM '/tmp/geodata/allCountries.txt';
+
+/* Put the data into the new tables */
+INSERT INTO location
+(geonameid, name, countrycode, featureclass, featurecode, featuretype, latitude, longitude, initial_weight)
+SELECT cast(geonameid as numeric),
+       name,
+       countrycode,
+       featureclass,
+       featurecode,
+       featureclass || '.' || featurecode,
+       cast(latitude as float),
+       cast(longitude as float),
+       0.0
+FROM raw_locations;
+
+INSERT INTO feature
+(featureclass, featurecode, code, name, description)
+SELECT substring(code FROM 1 FOR 1),
+       substring(code FROM 3 FOR (char_length(code) - 2)),
+       code,
+       name,
+       description
+FROM raw_feature_codes;
