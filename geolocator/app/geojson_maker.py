92aaa5c64dad3027bbcc2b5d5ecadf3c64b24d33
Weston Neal <wtneal@asu.edu>
2014-12-03 19:11:44 -0700

    Fixed the database call

--- geolocator/app/geojson_maker.py
+++ geolocator/app/geojson_maker.py
@@ -1,23 +1,17 @@
-#!/usr/bin/python
 import geojson
-
-#pip install geojson
-#pip install --upgrade geojson
-# to test: in terminal:
-	#python
-	#from geojson import Point
+from app.models import Location
 
 
 def FeaturePoint(lon, lat, weight, name):
 	geometry = { "type" : "Point", "coordinates" : [lat, lon] }
 	properties = { "weight" : weight, "name" : name }
-	#Feature takes in: id= "", geometry json, property json
+	# Feature takes in: id= "", geometry json, property json
 	feature = geojson.Feature(name, geometry, properties) 
 	return feature
 
 
 def RemoveDuplicatesFromList(l):
-	#unicode to string, assuming there will be no characters that lie outside of ascii range
+	# Unicode to string, assuming there will be no characters that lie outside of ascii range
 	stringlist = [str(x) for x in l] 
 	nodublicate_array = []
 	list(set(stringlist))
@@ -26,31 +20,19 @@ def RemoveDuplicatesFromList(l):
 
 
 def MakeGeoJsonElement(location, existing_locations):
-	#lookup x in database
-	
-	#necessary imports
-	##should move these to a separate file so that we can reuse them without having to rewrite them.
-	from sqlalchemy import create_engine
-	from sqlalchemy.orm import sessionmaker
-	from models import Location
-	import os
+	"""Gets the first hit that it gets with the given location name.
+	It only searches the name instead of the other parameters. Also because of first hit,
+	the accuracy is not very good. Will need to add additional logic for checking"""
 
-	#engine pointing to the db
-	engine = create_engine(os.getenv('DATABASE_URL','postgres://postgres@{0}/app'.format(os.getenv('DB_1_PORT_5432_TCP_ADDR'))), echo=True);
-	Session = sessionmaker(bind=engine)
-	session = Session()
-
-	#Gets the first hit that it gets with the given location name. It only searches the name instead of the other parameters. Also because of first hit, the accuracy is not very good. Will need to add additional logic for checking
-
-	loc = session.query(Location).filter(Location.name == location).first()
+	loc = Location.query.filter_by(name=location).first()
 	lon = 0.00
 	lat = 0.00
-	#if there is no match, the locations will be 0,0.	
+	# If there is no match, the locations will be 0,0.	
 	if not (loc is None):  
 		lon = loc.longitude
 		lat = loc.latitude
 
-	#weight calculations
+	# Weight calculations
 	weight = 0
 	for y in existing_locations:
 		if location == y:
@@ -61,11 +43,12 @@ def MakeGeoJsonElement(location, existing_locations):
 
 
 def MakeGeoJsonCollection(locations):
-	# --- remove duplicates
+	# Remove duplicates
 	noduplicates = RemoveDuplicatesFromList(locations)
-	# --- turn locations into FeaturePoints
+	# Turn locations into FeaturePoints
 	feature_array = []
 	for l in noduplicates:
 		feature_array.append(MakeGeoJsonElement(l, locations)) 
-	# --- Convert FeaturePoints List to FeatureCollection
+	# Convert FeaturePoints List to FeatureCollection
 	return geojson.FeatureCollection(feature_array)
+
