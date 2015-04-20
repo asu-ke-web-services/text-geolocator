08022709cb61b591f289a0bc3f8e4d7897ae9b27
Jack Workman <jworkman16@gmail.com>
2015-04-20 10:43:43 -0700

    wrote tests for __eq__ of models.Location

--- geolocator/app/tests/test_models.py
+++ geolocator/app/tests/test_models.py
@@ -67,6 +67,111 @@ class LocationTests(unittest.TestCase):
         assert self.Loc.longitude == longitude
         assert self.Loc.initial_weight == initial_weight
 
+    def test__eq__fail_on_Location(self):
+        """
+        Tests __eq__ - fails on isinstance(other, Location)
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = 'not a Location'
+        assert L1 != L2
+
+    def test__eq__fail_on_id(self):
+        """
+        Tests __eq__ - fails on id
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2.id = 4
+        assert L1 != L2
+
+    def test__eq__fail_on_location(self):
+        """
+        Tests __eq__ - fails on location
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("not Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_geonameid(self):
+        """
+        Tests __eq__ - fails on geonameid
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 21, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_name(self):
+        """
+        Tests __eq__ - fails on name
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "not Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_countrycode(self):
+        """
+        Tests __eq__ - fails on countrycode
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "not Z", "Y", "X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_featureclass(self):
+        """
+        Tests __eq__ - fails on featureclass
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "not Y", "X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_featurecode(self):
+        """
+        Tests __eq__ - fails on featurecode
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "not X", "W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_featuretype(self):
+        """
+        Tests __eq__ - fails on featuretype
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "not W", -1, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_latitude(self):
+        """
+        Tests __eq__ - fails on latitude
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -14, 1, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_longitude(self):
+        """
+        Tests __eq__ - fails on longitude
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 251, 9001)
+        assert L1 != L2
+
+    def test__eq__fail_on_initial_weight(self):
+        """
+        Tests __eq__ - fails on initial_weight
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001.1)
+        assert L1 != L2
+
+    def test__eq__pass(self):
+        """
+        Tests __eq__ with two equal Location objects
+        """
+        L1 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        L2 = Location("Endor", 1, "Endor", "Z", "Y", "X", "W", -1, 1, 9001)
+        assert L1 == L2
+
     def test_repre(self):
         """
         Tests the __repr__ function for correct output
